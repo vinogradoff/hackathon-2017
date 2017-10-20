cf890c51ed4c3d85aab78a26f9e2284979ec272a
Alexei Vinogradov <alexei@vinogradoff.de>
2017-10-20 15:58:13 +0300

    refactored

--- src/test/java/lt/hackathon/CheckoutTest.java
+++ src/test/java/lt/hackathon/CheckoutTest.java
@@ -3,15 +3,20 @@ package lt.hackathon;
 import lt.hackathon.pageobjects.checkout.OrderWidget;
 import lt.hackathon.steps.CheckoutSteps;
 import lt.hackathon.steps.LoginStep;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Condition.value;
 
 public class CheckoutTest {
 
+    @BeforeClass
+    public static void setup(){
+        LoginStep.login();
+    }
+
     @Test
     public void checkoutAndPay(){
-        LoginStep.login();
         CheckoutSteps.checkoutAndPay("SelenideTeam","Victoria Gerald","4532987522825600","12/2017");
         new OrderWidget().orderBtn.shouldHave(value("Place your order"));
         // we do not actually order
--- src/test/java/lt/hackathon/SearchTest.java
+++ src/test/java/lt/hackathon/SearchTest.java
@@ -4,15 +4,22 @@ import lt.hackathon.pageobjects.ProductPage;
 import lt.hackathon.pageobjects.SellersPage;
 import lt.hackathon.pageobjects.common.SearchPage;
 import lt.hackathon.steps.LoginStep;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTest {
 
+    @BeforeClass
+    public static void setup(){
+        open("/");
+        LoginStep.login();
+    }
+
+
     @Test
     public void filter()  {
-        open("/");
         new SearchPage().search("PlayStation 4 camera");
         new ProductPage().goToOtherSellers();
         new SellersPage().findBestSeller();
@@ -21,14 +28,12 @@ public class SearchTest {
 
     @Test
     public void search(){
-        LoginStep.login();
         new SearchPage().search("Playstation 4");
         new ProductPage().addToCart();
     }
 
     @Test
     public void searchCamera(){
-        LoginStep.login();
         new SearchPage().search("Playstation 4 camera");
         new ProductPage().addToCart();
     }
