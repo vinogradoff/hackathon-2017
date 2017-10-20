2ad485ae07855354165480b84d5637c9d685283a
Alexei Vinogradov <alexei@vinogradoff.de>
2017-10-20 13:13:54 +0300

    refactored

--- src/test/java/lt/hackathon/pageobjects/SelectShippingAddress.java
+++ src/test/java/lt/hackathon/pageobjects/SelectShippingAddress.java
@@ -2,7 +2,6 @@ package lt.hackathon.pageobjects;
 
 import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -12,10 +11,8 @@ public class SelectShippingAddress {
 
     public void deliverTo(String name){
         SelenideElement nameText=addressBook.$(byText(name));
-        nameText.shouldBe(visible);
         SelenideElement addressBox=nameText.closest("div").parent().parent();
-        addressBox.shouldBe(visible);
-        // click button
+        // click deliver button
         addressBox.$(".ship-to-this-address").click();
     }
 
